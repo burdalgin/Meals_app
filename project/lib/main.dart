a2b8cc8c9c043fc990db0ae0e92865b66825ee1c
Yaroslav <job.yaroslav@list.ru>
2023-04-07 00:15:11 +0300

    Start project| Home screen

---
+++ project/lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodySmall: const TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1), fontSize: 10),
+              bodyMedium: const TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1), fontSize: 12),
+              bodyLarge: const TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1), fontSize: 14),
+              titleSmall: const TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold),
+              titleMedium: const TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold),
+              titleLarge: const TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold),
+            ),
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Color.fromARGB(255, 175, 16, 219),
+          onPrimary: Color.fromARGB(255, 255, 255, 255),
+          secondary: Colors.amber,
+          onSecondary: Color.fromARGB(255, 255, 255, 255),
+          error: Colors.redAccent,
+          onError: Color.fromARGB(255, 255, 255, 255),
+          background: Colors.white,
+          onBackground: Color.fromARGB(255, 166, 158, 158),
+          surface: Colors.white,
+          onSurface: Color.fromARGB(255, 166, 158, 158),
+        ),
+      ),
+      home: const CategoriesScreen(), //устанавливаем домашнюю страницу
+    );
+  }
+}
